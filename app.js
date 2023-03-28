593e224b7a6c7def6be49f864af0979079599718
timmystroge <timmystroge75@gmail.com>
2023-03-28 21:59:15 +0100

    Quality Update

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
+const urlencodedparser = bodyParser.urlencoded({ extended: false });
 
+// handle index page request
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// handle /getWeather route request
+app.get("/getWeather", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// handle /getWeather post request
+app.post("/getWeather", urlencodedparser, function (req, res) {
+  //   get user input city name
+  let userCityName = req.body.cityName;
+
+  //   app endpoint details
   const appId = "930ecb3c80068e06f0123661a07e2cdc";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=lagos&appid=${appId}&units=metric`;
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${userCityName}&appid=${appId}&units=${unit}`;
 
   /* make request */
   https.get(url, function (response) {
-    console.log(response.statusCode);
+    console.log(response.statusCode); // 200, 404 etc
 
     response.on("data", function (data) {
       const weatherData = JSON.parse(data); /* Weather Data */
-      const temp = weatherData.main.temp; /* Weather Temperature */
-      const name = weatherData.name; /* City Name */
+
+      const temp = weatherData.main.temp; /* Weather Temperature = cloudy */
+      const name = weatherData.name; /* City Name = lagos*/
       const feels =
-        weatherData.main.feels_like; /* What the weaher feels like */
+        weatherData.main.feels_like; /* What the weaher feels like = string*/
       const description =
-        weatherData.weather[0].description; /* Weather description */
-      const icon = weatherData.weather[0].icon; /* Current weather icon */
+        weatherData.weather[0].description; /* Weather description = string*/
+      const icon = weatherData.weather[0].icon; /* Current weather icon  = int*/
       const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
       res.write(
         `<h1>The temperature in ${name} is ${temp} degree celcius.</h1>`
       );
       res.write(`<p>The weather is currently ${description}</p>`);
-      res.write(`<p><i>The weather feels like ${feels}</i></p>`);
+      res.write(`<p><i>Today weather feels ${feels}</i></p>`);
       res.write(`<img src="${imageUrl}">`);
       /* send data to user */
       res.send();
