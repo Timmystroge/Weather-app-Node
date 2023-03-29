08ba3c47b9b76cffd50c874fe834f48f48abe860
timmystroge <timmystroge75@gmail.com>
2023-03-29 06:52:47 +0100

    add dotenv, env vars

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+/* dotenv to load .env files */
+require("dotenv").config();
 
 const app = express();
 const urlencodedparser = bodyParser.urlencoded({ extended: false });
@@ -12,7 +14,7 @@ app.get("/", function (req, res) {
 
 // handle /getWeather route request
 app.get("/getWeather", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  res.redirect("/");
 });
 
 // handle /getWeather post request
@@ -21,37 +23,50 @@ app.post("/getWeather", urlencodedparser, function (req, res) {
   let userCityName = req.body.cityName;
 
   //   app endpoint details
-  const appId = "930ecb3c80068e06f0123661a07e2cdc";
+
+  const appId = process.env.APIURL;
   const unit = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${userCityName}&appid=${appId}&units=${unit}`;
 
   /* make request */
   https.get(url, function (response) {
-    console.log(response.statusCode); // 200, 404 etc
+    const requestStatus = response.statusCode;
 
     response.on("data", function (data) {
-      const weatherData = JSON.parse(data); /* Weather Data */
+      if (requestStatus === 200) {
+        const weatherData = JSON.parse(data); /* Weather Data */
 
-      const temp = weatherData.main.temp; /* Weather Temperature = cloudy */
-      const name = weatherData.name; /* City Name = lagos*/
-      const feels =
-        weatherData.main.feels_like; /* What the weaher feels like = string*/
-      const description =
-        weatherData.weather[0].description; /* Weather description = string*/
-      const icon = weatherData.weather[0].icon; /* Current weather icon  = int*/
-      const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-      res.write(
-        `<h1>The temperature in ${name} is ${temp} degree celcius.</h1>`
-      );
-      res.write(`<p>The weather is currently ${description}</p>`);
-      res.write(`<p><i>Today weather feels ${feels}</i></p>`);
-      res.write(`<img src="${imageUrl}">`);
-      /* send data to user */
-      res.send();
+        const temp = weatherData.main.temp; /* Weather Temperature = cloudy */
+        const name = weatherData.name; /* City Name = lagos*/
+        const feels =
+          weatherData.main.feels_like; /* What the weaher feels like = string*/
+        const description =
+          weatherData.weather[0].description; /* Weather description = string*/
+        const icon =
+          weatherData.weather[0].icon; /* Current weather icon  = int*/
+        const imageUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        res.write(`<p>The Weather Condition in ${userCityName}</p>`);
+        res.write(
+          `<h1>The temperature in ${name} is ${temp} degree celcius.</h1>`
+        );
+        res.write(`<p>The weather is currently ${description}</p>`);
+        res.write(`<p><i>Today weather feels ${feels}</i></p>`);
+        res.write(`<img src="${imageUrl}">`);
+
+        /* send data to user */
+        res.send();
+      } else {
+        res.send("Oops! Something went wrong!, Please try again");
+      }
     });
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+app.listen(3000 || 3000, () => {
+  console.log(
+    "server is running on port 3000"
+  );
 });
+
+// Export the Express API
+module.exports = app;
